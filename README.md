f7ee717cc2f8ee68de5f08119f39982d977f8182
Fillipe Diord <fillipe.diord@hotmail.com>
2021-10-04 15:31:38 -0300

    Project created for download from a private or public github repository

---
+++ README.md
@@ -0,0 +1,8 @@
+ #  Para ter acesso e conseguir baixar os arquivos de um determinado repositório 
+ #  privado, é necessário entrar no github e gerar um token para dar disponibilidade de acesso
+ #  para quem deseja fazer o download, para isso é preciso entrar nas settings
+ #  depois escolher a opção developer settings, após ter carregado escolher a opção 
+ #  Personal acess tokens e após isso clicar em Generate new Token, coloque a senha caso pedir,
+ #  coloque um nome para o token, quanto tempo de duração do token, o que o token vai ter acesso e após
+ #  isso clicar em generate token, ele será gerado e aí basta copiar o token e colocar no Authorization
+ #  que fica dentro do header de cada requisição
---
+++ src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+// request para listar os arquivos de um repositório
+const listArchives = {
+  method: 'GET',
+  url: 'https://api.github.com/repos/{USER}/{NAME-REPOSITORY}/contents/',
+  headers: {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: 'token {TOKEN}'
+  }
+};
+
+axios.request(listArchives).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+
+// request para listar as especificações de um repositório
+const repositoryAccess = {
+  method: 'GET',
+  url: 'https://api.github.com/repos/{USER}/{NAME-REPOSITORY}',
+  headers: {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: 'token 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'
+  }
+};
+
+axios.request(repositoryAccess).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+
+// request para fazer download de um repositório 
+/* Caso queira fazer download em formato zip usar zipball na url */
+/* Caso queira fazer download em formato tar usar tarball na url */
+const downRepository = {
+  method: 'GET',
+  url: 'https://api.github.com/repos/{USER}/{NAME-REPOSITORY}/zipball/main',
+  headers: {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: 'token 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'
+  }
+};
+
+axios.request(downRepository).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+app.listen(3333);
+
